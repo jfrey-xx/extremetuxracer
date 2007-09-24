414402042d6997b227d08fc54b56a0f203532c95
hamishmorrison <hamishmorrison@0420edf4-82e4-42fc-9478-35b55e6d67a3>
2007-09-24 18:14:26 +0000

    Dutch (NL) translation updated

--- branches/code_cleanup/data/translations/nl_NL.tcl
+++ branches/code_cleanup/data/translations/nl_NL.tcl
@@ -1,20 +1,22 @@
 #Translation maintained by Asciimonster
 #Please e-mail Comments & Corrections to asciimonster@gmx.net
 
+#Corrected by Jitse De Wandel
+
 #common strings
 pp_translate_string "Back" "Terug"
-pp_translate_string "PRESS ANY KEY TO START" "DRUK EEN TOETS OM TE STARTEN"
+pp_translate_string "PRESS ANY KEY TO START" "DRUK OP EEN TOETS OM TE STARTEN"
 
 #game_type_select
 pp_translate_string "Enter an event" "Start een toernooi"
-pp_translate_string "Practice" "Training"
-pp_translate_string "Configuration" "Configuratie"
+pp_translate_string "Practice" "Oefening"
+pp_translate_string "Configuration" "Instellingen"
 pp_translate_string "Credits" "Auteurs"
-pp_translate_string "Quit" "Einde"
+pp_translate_string "Quit" "Afsluiten"
 
 #configuration
-pp_translate_string "Graphics" "Beeld"
-pp_translate_string "Video" "Video"
+pp_translate_string "Graphics" "Weergave"
+pp_translate_string "Video" "Beeldscherm"
 pp_translate_string "Audio" "Geluid"
 pp_translate_string "Keyboard" "Toetsenbord"
 pp_translate_string "Joystick" "Joystick"
@@ -26,15 +28,15 @@ pp_translate_string "Ok" "Ok"
 #graphics configuration
 pp_translate_string "Graphics Configuration" "Visuele instellingen"
 pp_translate_string "Language:" "Taal:"
-pp_translate_string "Show UI Snow:" "Toon sneeuw in menu's:"
-pp_translate_string "Display FPS:" "Ververssnelheid monitor weergeven:"
+pp_translate_string "Show UI Snow:" "Sneeuw in menus weergeven:"
+pp_translate_string "Display FPS:" "Beelden per seconden weergeven:"
 pp_translate_string "Display Progress Bar:" "Voortgangsbalk weergeven:"
-pp_translate_string "Draw Fog:" "Toon mist:"
-pp_translate_string "Reflections:" "Reflecties:"
-pp_translate_string "Shadows:" "Schaduw:"
+pp_translate_string "Draw Fog:" "Mist weergeven:"
+pp_translate_string "Reflections:" "Reflecties weergeven:"
+pp_translate_string "Shadows:" "Schaduw weergeven:"
 
 #video configuration
-pp_translate_string "Video Configuration" "Video instellen"
+pp_translate_string "Video Configuration" "Beeldscherm instellen"
 pp_translate_string "Resolution:" "Resolutie:"
 pp_translate_string "Bits Per Pixel:" "Bits per pixel:"
 pp_translate_string "Fullscreen:" "Volledig scherm:"
@@ -67,7 +69,7 @@ pp_translate_string "Enable Joystick" "Joystick activeren"
 
 #race select
 pp_translate_string "Race!" "Start!"
-pp_translate_string "Select a race" "Kies een wedstrijd"
+pp_translate_string "Select a race" "Kies een race"
 pp_translate_string "Contributed by:" "Auteur:"
 pp_translate_string "Unknown" "Onbekend"
 pp_translate_string "Time:" "Tijd:"
@@ -84,10 +86,10 @@ pp_translate_string "You must complete this cup next" "Eerst moet dit toernooi v
 pp_translate_string "You cannot enter this cup yet" "Je mag nog niet aan dit toernooi meedoen"
 
 #event race select
-pp_translate_string "You don't have any lives left." "Je hebt geen levens meer"
-pp_translate_string "Race won! Your result:" "Wedstrijd gewonnen! Jouw resultaat:"
-pp_translate_string "Needed to advance:" "Benodigd:"
-pp_translate_string "You can't enter this race yet." "Je mag nog niet aan deze wedstrijd meedoen"
+pp_translate_string "You don't have any lives left." "Je hebt geen levens meer."
+pp_translate_string "Race won! Your result:" "Race gewonnen! Jouw resultaat:"
+pp_translate_string "Needed to advance:" "Doelen:"
+pp_translate_string "You can't enter this race yet." "Je mag nog niet aan deze race meedoen."
 
 #loading
 pp_translate_string "Loading, Please Wait..." "Even geduld alstublieft..."
@@ -97,7 +99,7 @@ pp_translate_string "Resume" "Doorgaan"
 pp_translate_string "Paused" "Pauze"
 
 #race over
-pp_translate_string "Race Over" "Wedstrijd afgelopen"
+pp_translate_string "Race Over" "Race afgelopen"
 pp_translate_string "Time: %02d:%02d.%02d" "Tijd: %02d:%02d.%02d"
 pp_translate_string "Herring: %3d" "Haringen: %3d"
 pp_translate_string "Score: %6d" "Score: %6d"
@@ -105,10 +107,10 @@ pp_translate_string "Max speed: %3d km/h" "Maximale snelheid: %3d km/h"
 pp_translate_string "Was flying: %.01f %% of time" "Gevlogen: %.01f %% van de tijd"
 pp_translate_string "Race aborted" "Wedstrijd afgebroken"
 pp_translate_string "You beat your best score!" "Je hebt je beste tijd verbeterd!"
-pp_translate_string "Congratulations! You won the event!" "Gefeliciteerd! Je hebt de wedstrijd gewonnen"
-pp_translate_string "Congratulations! You won the cup!" "Gefeliciteerd! Je hebt het toernooi gewonnen"
+pp_translate_string "Congratulations! You won the event!" "Gefeliciteerd! Je hebt de wedstrijd gewonnen!"
+pp_translate_string "Congratulations! You won the cup!" "Gefeliciteerd! Je hebt het toernooi gewonnen!"
 pp_translate_string "You advanced to the next race!" "Je mag meedoen aan de volgende wedstrijd!"
-pp_translate_string "You didn't advance." "Je hebt het niet gehaald"
+pp_translate_string "You didn't advance." "Je hebt het niet gehaald."
 
 #highscore
 pp_translate_string "Highscore" "Scorebord"
@@ -121,7 +123,7 @@ pp_translate_string "4:th" "4:de"
 pp_translate_string "5:th" "5:de"
 pp_translate_string "6:th" "6:de"
 pp_translate_string "7:th" "7:de"
-pp_translate_string "8:th" "8:de"
+pp_translate_string "8:th" "8:ste"
 pp_translate_string "9:th" "9:de"
 pp_translate_string "10:th" "10:de"
 
